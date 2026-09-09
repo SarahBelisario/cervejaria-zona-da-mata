fdaac6dd9e4b23c496b36a4fb14e0fa6b2d4769a
Sarah Belisario <sarahhaide89@gmail.com>
2026-09-09 15:36:16 -0300

    Adiciona página sobre e compartilha menu responsivo entre páginas
    - Centraliza o JavaScript do cabeçalho no global.js
    - Atualiza os links de navegação
    - Corrige o sublinhado da página atual

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,1 @@
 import "./components/global.js";
-const pagina = document.body.dataset.pagina;
-
-switch (pagina) {
-    case "home":
-        await import("./components/global.js");
-        break;
-}
